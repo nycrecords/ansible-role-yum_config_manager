4b0a93d387fdd84801f45eef2b44def4c614b828
Joel Castillo <jocastillo@records.nyc.gov>
2019-11-22 11:34:36 -0500

    Add test

--- molecule/resources/tests/test_default.py
+++ molecule/resources/tests/test_default.py
@@ -6,3 +6,18 @@ import pytest
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ["MOLECULE_INVENTORY_FILE"]
 ).get_hosts("all")
+
+@pytest.fixture(scope="module")
+def enabled_repos(host):
+    with host.sudo():
+        stdout = host.check_output("yum repolist enabled")
+
+    enabled_repos = []
+    for line in stdout.splitlines():
+        if line.lower().startswith("!"):
+            enabled_repos.append(line.split()[0])
+    return enabled_repos
+
+
+def test_repos_enabled(expected_repos, enabled_repos):
+    assert set(None) == set(enabled_repos)
